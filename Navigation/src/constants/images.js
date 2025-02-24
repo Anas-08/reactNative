163eb6d96dd96cba2030fdc9a49ca4b0859a37a8
Anas-08 <anasmansuri328@gmail.com>
2025-02-24 20:27:51 +0530

    resp test

--- Navigation/src/constants/images.js
+++
@@ -1,7 +0,0 @@
-const path = "../assets/images/icons/"
-
-export default imgResponsive = {
-    babyBackground: require(`${path}babyBackground.jpg`),
-    hideIcon: require(`${path}visibilityOff.png`),
-    showIcon: require(`${path}visibilityOn.png`),
-}
