08c2420eedb00eb0610ae2329b92beef8e779579
Anas-08 <anasmansuri328@gmail.com>
2025-02-24 20:33:54 +0530

    resp test

---
+++ Navigation/src/constants/navigationResponsiveString.js
@@ -0,0 +1,10 @@
+export default {
+    login: "LoginRes",
+    register: "RegisterResponsive",
+    chooseAccount: "ChooseAccount",
+    forgotPassword: "ForgotPassword",
+    setPassword: "SetPassword",
+    
+    home: "HomeRes",
+
+}
